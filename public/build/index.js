cf342c05b2d13082a5372b7421eb27f5c7896a9e
David <nizz3hx@protonmail.com>
2021-03-20 14:05:07 +0000

    Implemented API, Style, and more HTML

---
+++ public/build/index.js
@@ -0,0 +1,29 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase()
+  console.log(choice)
+  const url = `https://rickandmortyapi.com/api/character/?name=${choice}`
+
+  fetch(url)
+
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        document.querySelector("#outputs").style.visibility = "visible"
+        console.log(data)
+        document.querySelector('#charName').innerText = data.results[0].name
+        document.querySelector('#charImg').src = data.results[0].image
+        document.querySelector('#stat').innerText = data.results[0].status
+        document.querySelector('#species').innerText = data.results[0].species
+        document.querySelector('#gender').innerText = data.results[0].gender
+        if(data.results[0].type === "" || data.results[0].type === null){
+          document.querySelector('#type').remove()
+      }else{
+        document.querySelector('#ty').innerText = data.results[0].type
+      }
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+
+      });
+      }

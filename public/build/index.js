6d2448702a86fdbf61b8b3340670a5728f184fcd
David <nizz3hx@protonmail.com>
2021-03-29 15:05:11 +0100

    Updated to make Origin and Location work

--- public/build/index.js
+++ public/build/index.js
@@ -10,12 +10,15 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         document.querySelector("#outputs").style.visibility = "visible"
-        console.log(data)
+        // console.log(data)
         document.querySelector('#charName').innerText = data.results[0].name
         document.querySelector('#charImg').src = data.results[0].image
         document.querySelector('#stat').innerText = data.results[0].status
         document.querySelector('#species').innerText = data.results[0].species
         document.querySelector('#gender').innerText = data.results[0].gender
+        document.querySelector('#orig').innerText = data.results[0].origin.name
+        document.querySelector('#loc').innerText = data.results[0].location.name
+
         if(data.results[0].type === "" || data.results[0].type === null){
           document.querySelector('#type').remove()
       }else{
@@ -24,6 +27,5 @@ function getFetch(){
       })
       .catch(err => {
           console.log(`error ${err}`)
-
       });
       }
